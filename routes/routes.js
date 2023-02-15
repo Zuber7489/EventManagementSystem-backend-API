ef68ec237b00fc86f4a1732c0d1309c13d9df5e2
juned-shaikh <juned.shaikh.7415@gmail.com>
2023-02-15 22:19:27 +0530

    changes

--- routes/routes.js
+++ routes/routes.js
@@ -482,7 +482,7 @@ router.get('/eventlist/data', (req,res)=>{
 
 
 //Get User Event Api
-router.get('/eventlist/userdata',async (req,res)=>{
+router.get('/eventlist/userdata',isAuthenticate,async (req,res)=>{
    try {
    let data = await Event.findOne({userId: req.body.userId})
    res.send(data)
